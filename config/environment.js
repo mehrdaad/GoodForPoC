5bb5789a9cf5237a1baf8cf3f779ddc476c949b7
Jacky Alciné <yo@jacky.wtf>
2017-09-24 14:54:28 -0700

    fix(ember): Drop prototypes.

--- config/environment.js
+++ config/environment.js
@@ -12,10 +12,7 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+      EXTEND_PROTOTYPES: false
     },
 
     APP: {
@@ -41,10 +38,6 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-
   }
 
   return ENV;
